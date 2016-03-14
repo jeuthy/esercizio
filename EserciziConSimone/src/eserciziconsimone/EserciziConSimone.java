155b8c8f99ae9512741eb4576586a4e1e64820a3
jeuthy <stefano.meddi93@gmail.com>
2016-03-15 00:29:04 +0100

    prova commit 1

---
+++ EserciziConSimone/src/eserciziconsimone/EserciziConSimone.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziconsimone;
+
+/**
+ *
+ * @author stefano
+ */
+public class EserciziConSimone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //prova commit 1    
+    // TODO code application logic here
+    }
+    
+}
